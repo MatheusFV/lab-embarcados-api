076f7e733f64b26bc0a83a482919d246a3dd9977
MatheusFV <matheus.venosa@usp.br>
2018-10-18 15:32:30 -0300

    adding the socket init

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,17 @@ Server.set('port', port);
 console.log(`Server listening on port ${port}`);
 
 const server: http.Server = http.createServer(Server);
+const io: any = require('socket.io')(server);
 
 server.listen(port);
+const net: any = require('net');
+
+const serverSocket: any = net.createServer((socket) => {
+    socket.write('Echo server\r\n');
+    socket.pipe(socket);
+});
+
+serverSocket.listen(1337, '127.0.0.1');
+
 server.on('error', error => serverHandlers.onError(error, port));
 server.on('listening', serverHandlers.onListening.bind(server));
